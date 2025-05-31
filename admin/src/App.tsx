0775f0bf86cc708e370e6a43dbab7cbe1e141d1b
noorjsdivs <noor.jsdivs@gmail.com>
2025-05-31 11:25:44 +0600

    project setup done

---
+++ admin/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="">Admin application</div>;
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,8 @@
+const x = 2;
+const y = 5;
+
+const total = x + y;
+const value = y - x;
+
+console.log(total, value);
+console.log("hello");
